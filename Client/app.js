b51fed7624cb57bf6131196ac1700bdfee538134
brookebachman00 <bbachman1994@gmail.com>
2020-11-13 16:33:22 -0700

    I am formatting the page with flex

--- Client/app.js
+++ Client/app.js
@@ -1,24 +1,66 @@
 
 const apiKey = '0b53ac0f940893f55db63af93f3ada83'
 const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
-const data = {};
-
-document.getElementById('form').addEventListener('submit', performAction);
+const results = []
+document.getElementById('generate').addEventListener('click', performAction);
+const weather = document.getElementById("entryHolder")
+const resultsDiv = document.createElement("div")
+resultsDiv.id = "results"
 
 function performAction(event){
     event.preventDefault();
-    const zipcode = document.getElementById('zipcode').value
+    const zipcode = document.getElementById('feelings').value
     getZipode(baseURL, zipcode, apiKey)
 }
 
-
 const getZipode = async (baseURL, zipcode, apiKey) => {
-    const response = await fetch(baseURL+zipcode+",us"+"&appid="+apiKey) 
+    const response = await fetch(baseURL+zipcode+",us"+"&appid="+apiKey+"&units=imperial") 
     try {
         const data = await response.json();
-        console.log(data);
+        results.push(data)
+        console.log(results[0])
+        createLis();
+        updateFrontend(results);
     } catch (error){
         console.log(error, "this is the error")
     }
-
 }
+const createLis = () => {
+    for (let i = 0; i < 3; i++){
+        const p = document.createElement('p')
+        p.id = i
+        weather.appendChild(p);
+    }
+}
+
+function updateFrontend(results){
+    weather.appendChild(resultsDiv);
+    const clouds = document.getElementById("clouds")
+    const image = document.createElement("img")
+    const card = document.getElementById("results")
+   const temp = document.getElementById("0");
+   const date = document.getElementById("1");
+   const content = document.getElementById("2");
+//    date.innerText = 
+   card.appendChild(temp)
+   card.appendChild(clouds);
+   card.appendChild(image);
+
+    temp.innerText = results[0].main.temp.toFixed() + " degrees"
+
+    if (results[0].clouds.all === 0){
+        clouds.innerText = "There are no clouds in the sky today ☀"	
+    } else {
+        clouds.innerText = "It is a cloudy day ☁"
+    }
+    if (results[0].weather[0].description === "clear sky"){
+        image.src = '../images/sun.jpg'
+    } else if (results[0].weather[0].description === "broken clouds"){
+        image.src = '../images/clouds.jpg'
+    } else {
+        image.src = '../images/rain.jpg'
+    }    
+    
+    
+}
+
